82ea4337291986b163f7ea4f484e008a9999048f
ehardacre <hardacre.ethan@gmail.com>
2017-11-11 07:06:50 -0800

    Final

---
+++ rotation/rotation/PieceView.swift
@@ -0,0 +1,36 @@
+////
+////  PieceView.swift
+////  rotation
+////
+////  Created by Ethan Hardacre on 11/11/17.
+////  Copyright © 2017 Ethan Hardacre. All rights reserved.
+////
+//
+//import UIKit
+//
+//class PieceView: UIImageView{
+//
+//    var pieceType = ""
+//    
+//    
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        switch pieceType {
+//        case "S":
+//            self.image = #imageLiteral(resourceName: "S")
+//        case "Z":
+//            self.image = #imageLiteral(resourceName: "Z")
+//        case "I":
+//            self.image = #imageLiteral(resourceName: "I")
+//        case "J":
+//            self.image = #imageLiteral(resourceName: "J")
+//        case "O":
+//            self.image = #imageLiteral(resourceName: "O")
+//        case "T":
+//            self.image = #imageLiteral(resourceName: "T")
+//        default:
+//            print("no type")
+//        }
+//    }
+//    
+//}
---
+++ rotation/rotationTests/rotationTests.swift
@@ -0,0 +1,36 @@
+//
+//  rotationTests.swift
+//  rotationTests
+//
+//  Created by Ethan Hardacre on 11/4/17.
+//  Copyright © 2017 Ethan Hardacre. All rights reserved.
+//
+
+import XCTest
+@testable import rotation
+
+class rotationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
