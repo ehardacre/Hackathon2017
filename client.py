965a97f230b0f8130d45b0cbb1b225d14de3add4
Kevin Lee <kevinsangholee@gmail.com>
2017-11-10 20:26:41 -0800

    server2

--- client.py
+++ client.py
@@ -9,5 +9,5 @@ clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 clientSock.connect((host,port))
 
 while True:
-    userInput = input("Enter a command: ")
+    userInput = raw_input("Enter a command: ")
     clientSock.send(userInput.encode('utf-8'))
